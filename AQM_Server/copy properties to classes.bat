17570f1506bbe3fe8a93db6f0f927179a893cf98
vivienwu <wuweiwei8889@gmail.com>
2014-03-14 11:06:18 -0700

    null

--- AQM_Server/copy properties to classes.bat
+++
@@ -1,4 +0,0 @@
-cd D:\ASUProject\AQM_Server\properties
-d:
-mkdir D:\ASUProject\AQM_Server\WEB-INF\classes\properties
-copy * D:\ASUProject\AQM_Server\WEB-INF\classes\properties\
--- AQM_Server/sql/echodata.sql
+++
@@ -1,7 +0,0 @@
--- Script to select * all tuples from DB, to be run from ij tool
--- connect 'jdbc:derby:D:/projects/AQM_dev/derby_home/aspiradb';
-connect 'jdbc:derby:derby_home/aspiradb';
-select * from "APP"."PARTICLEREADING";
-select * from "APP"."SERVERPUSHEVENTLOG";
-disconnect;
-exit;
